41ae26d06ae859987fac36b8820e9b65d9529401
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2023-02-05 19:24:55 +0300

    language support implemented

---
+++ web/src/common/i18n/resources.ts
@@ -0,0 +1,14 @@
+// Common
+import wmsEN from "./en/wms.json";
+import wmsTR from "./tr/wms.json";
+
+const resources = {
+  en: {
+    wms: wmsEN,
+  },
+  tr: {
+    wms: wmsTR,
+  },
+};
+
+export default resources;
